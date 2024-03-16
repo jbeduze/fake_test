d336d70f5777a550b717566b72c8d9b8875a8941
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-03-16 17:26:44 +0000

    added pages and styling

--- Home.py
+++ Home.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
+# with open( "config/1style.css" ) as css:
+#     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
+# with open( "config/style.css" ) as css:
+#     st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
+
 st.title("Hello World")
 
+st.write("Hi Edgar")
